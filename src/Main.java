b0892b0b4ac3fe3d211eef783ad5b605ae22158f
T9 <veronggg5@gmail.com>
2025-10-04 18:29:01 +0700

    fix: for Sonar

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,8 @@ public class Main{
     public static void main(String[] args) {
 
         final int N = 5;
-        int pastInt = 0;
         Random randomInt = new Random();
         ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Integer> uniqlist = new ArrayList<>();
-        ArrayList<Integer> duplicatlist = new ArrayList<>();
-        int[] arr;
 
         System.out.println("//////////////// 1.1,1.2 //////////////////////");
         for (int i = 0; i < N; i++) {
@@ -28,6 +24,91 @@ public class Main{
         System.out.println("////////////////// 1.5 ////////////////////");
         Collections.shuffle(list);
         System.out.println(list);
+
+        task6(list);
+
+        System.out.println("///////////////// 2 ////////////////////");
+        PrimesGeneratorTest test2 = new PrimesGeneratorTest();
+
+        test2.test(4,6);
+
+        System.out.println("///////////////// 3 ////////////////////");
+
+        ArrayList<Human> humanList = new ArrayList<>();
+        humanList.add(new Human("Anna","Johnson",15));
+        humanList.add(new Human("Drew","Johnson",23));
+        humanList.add(new Human("Hailey","Hernandez",13));
+        humanList.add(new Human("Linda","Roberts",30));
+        humanList.add(new Human("Jenny","Thomas",45));
+
+        HashSet<Human> hashSetHumans = new HashSet<>(humanList); //In random order
+        System.out.println(hashSetHumans);
+
+        LinkedHashSet<Human> linkedHashSetHumans = new LinkedHashSet<>(humanList); //In the order of the original array
+        System.out.println(linkedHashSetHumans);
+
+        TreeSet<Human> treeSetHumans = new TreeSet<>(humanList); //Sorts by compareTo in Human(Name)
+        System.out.println(treeSetHumans);
+
+        TreeSet<Human> treeSetHumansLastName = new TreeSet<>(new HumanComparatorByLastName()); //Sorts by compareTo in HumanComparatorByLastName(LastName)
+        treeSetHumansLastName.addAll(humanList);
+        System.out.println(treeSetHumansLastName);
+
+        //Sorts by age
+        TreeSet<Human> treeSetHumansage = new TreeSet<>((o1, o2) -> Integer.compare(o1.age,o2.age));
+        treeSetHumansage.addAll(humanList);
+        System.out.println(treeSetHumansage);
+
+        System.out.println("///////////////// 4 ////////////////////");
+
+        String text = "Words words name counts name name coUnts avey";
+        String[] words = text.split("\\s+");
+        HashMap<String,Integer> map = new HashMap<>();
+
+        for (String s : words) {
+
+            String word = s.toLowerCase();
+            if (map.get(word) != null) {
+
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        System.out.println(map);
+
+        System.out.println("///////////////// 5 ////////////////////");
+
+        Map<String,Integer> studentsScore = new HashMap<>();
+        Map<Integer,String> scoreStudents = new HashMap<>();
+        studentsScore.put("Drew",5);
+        studentsScore.put("Alan",4);
+        studentsScore.put("Janet",5);
+        studentsScore.put("Bryan",3);
+        studentsScore.put("Anna",4);
+
+        for (Map.Entry<String,Integer> entry : studentsScore.entrySet()){
+
+            String names;
+            if (scoreStudents.get(entry.getValue()) != null){
+
+                names = scoreStudents.get(entry.getValue()) + "; " + entry.getKey();
+            }
+            else{
+                names = entry.getKey();
+            }
+            scoreStudents.put(entry.getValue(), names);
+        }
+        System.out.println(studentsScore);
+        System.out.println(scoreStudents);
+    }
+
+    public static void task6(ArrayList<Integer> list){
+
+        int pastInt = 0;
+        ArrayList<Integer> uniqlist = new ArrayList<>();
+        ArrayList<Integer> duplicatlist = new ArrayList<>();
+        int[] arr;
 
         System.out.println("////////////////// 1.6 ////////////////////");
         for (int i = 0; i <= list.size(); i++) {
@@ -75,84 +156,5 @@ public class Main{
         for (int i : list) {
             System.out.println(i + " = " + Collections.frequency(list, i));
         }
-
-        System.out.println("///////////////// 2 ////////////////////");
-        PrimesGeneratorTest test2 = new PrimesGeneratorTest();
-
-        test2.test(4,6);
-
-        System.out.println("///////////////// 3 ////////////////////");
-
-        ArrayList<Human> humanList = new ArrayList<>();
-        humanList.add(new Human("Anna","Johnson",15));
-        humanList.add(new Human("Drew","Johnson",23));
-        humanList.add(new Human("Hailey","Hernandez",13));
-        humanList.add(new Human("Linda","Roberts",30));
-        humanList.add(new Human("Jenny","Thomas",45));
-
-        HashSet<Human> hashSetHumans = new HashSet<>(humanList); //In random order
-        System.out.println(hashSetHumans);
-
-        LinkedHashSet<Human> linkedHashSetHumans = new LinkedHashSet<>(humanList); //In the order of the original array
-        System.out.println(linkedHashSetHumans);
-
-        TreeSet<Human> treeSetHumans = new TreeSet<>(humanList); //Sorts by compareTo in Human(Name)
-        System.out.println(treeSetHumans);
-
-        TreeSet<Human> treeSetHumansLastName = new TreeSet<>(new HumanComparatorByLastName()); //Sorts by compareTo in HumanComparatorByLastName(LastName)
-        treeSetHumansLastName.addAll(humanList);
-        System.out.println(treeSetHumansLastName);
-
-        TreeSet<Human> treeSetHumansage = new TreeSet<>(new Comparator<>() {  //Sorts by age
-            @Override
-            public int compare(Human o1, Human o2) {
-                return Integer.compare(o1.age,o2.age);
-            }
-        });
-        treeSetHumansage.addAll(humanList);
-        System.out.println(treeSetHumansage);
-
-        System.out.println("///////////////// 4 ////////////////////");
-
-        String text = "Words words name counts name name coUnts avey";
-        String[] words = text.split("\\s+");
-        HashMap<String,Integer> map = new HashMap<>();
-
-        for (String s : words) {
-
-            String word = s.toLowerCase();
-            if (map.get(word) != null) {
-
-                map.put(word, map.get(word) + 1);
-            } else {
-                map.put(word, 1);
-            }
-        }
-        System.out.println(map);
-
-        System.out.println("///////////////// 5 ////////////////////");
-
-        Map<String,Integer> studentsScore = new HashMap<>();
-        Map<Integer,String> scoreStudents = new HashMap<>();
-        studentsScore.put("Drew",5);
-        studentsScore.put("Alan",4);
-        studentsScore.put("Janet",5);
-        studentsScore.put("Bryan",3);
-        studentsScore.put("Anna",4);
-
-        for (Map.Entry<String,Integer> entry : studentsScore.entrySet()){
-
-            String names;
-            if (scoreStudents.get(entry.getValue()) != null){
-
-                names = scoreStudents.get(entry.getValue()) + "; " + entry.getKey();
-            }
-            else{
-                names = entry.getKey();
-            }
-            scoreStudents.put(entry.getValue(), names);
-        }
-        System.out.println(studentsScore);
-        System.out.println(scoreStudents);
     }
 }
--- src/PrimesGenerator.java
+++ src/PrimesGenerator.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class PrimesGenerator {
+
+    private final Random randomInt = new Random();
+
     public ArrayList<Integer> start(int N){
 
-        Random randomInt = new Random();
         ArrayList<Integer> simpleNumbers = new ArrayList<Integer>();
 
         for (int i = 0; i < N; i++) {
