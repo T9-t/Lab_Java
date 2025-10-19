30c778ddc546f79cd63cfdb32932bcd76cc5f116
T9 <veronggg5@gmail.com>
2025-10-19 12:53:53 +0700

    feat: added task 3 classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
+import model.Order;
 import model.ShoeWarehouse;
 
+import java.security.SecureRandom;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static model.ShoeWarehouse.productType;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        final int N = 10;
+        ExecutorService service = Executors.newFixedThreadPool(2);
 
         Thread evenThread = new EvenThread();
         Thread oddThread = new Thread(new OddRunnable());
@@ -9,11 +19,32 @@ public class Main {
         evenThread.start();
         oddThread.start();
 
-        ShoeWarehouse warehouse = new ShoeWarehouse();
-        Thread producer = new Thread(new Producer(warehouse,10));
+        ShoeWarehouse warehouse = new ShoeWarehouse(service);
+        Thread producer = new Thread(new Producer(warehouse,N));
         Thread consumer = new Thread(new Consumer(warehouse));
 
         producer.start();
         consumer.start();
+
+        for (int i = 0; i < N; i++) {
+            service.submit(() -> {
+                try {
+                   warehouse.receiveOrder(randomOrder());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            warehouse.fulfillOrderService();
+        }
+        service.shutdown();
+    }
+    public static Order randomOrder(){
+
+        SecureRandom randomInt = new SecureRandom();
+        int id = randomInt.nextInt(1000)
+        ,shoesTypeNumber = randomInt.nextInt(productType.size() - 1)
+        ,quantity = randomInt.nextInt(100);
+
+        return new Order( id,productType.get(shoesTypeNumber),quantity);
     }
 }
--- src/main/java/model/ShoeWarehouse.java
+++ src/main/java/model/ShoeWarehouse.java
@@ -2,12 +2,18 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 public class ShoeWarehouse {
 
     public final static List<String> productType = List.of("Sneakers","Boots","Crocs","Sandals");
     private List<Order> orders = new ArrayList<>();
     private final Object lock = new Object();
+    private ExecutorService service;
+
+    public ShoeWarehouse(ExecutorService service){
+        this.service = service;
+    }
 
     public void receiveOrder(Order order) throws InterruptedException {
 
@@ -31,7 +37,27 @@ public class ShoeWarehouse {
             lock.notifyAll();
 
             System.out.println(this);
-        }
+        };
+    }
+    public void fulfillOrderService() {
+
+        service.submit(() -> {
+
+            synchronized (lock) {
+                while (orders.isEmpty()) {
+                    try {
+                        lock.wait();
+
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                orders.removeFirst();
+                lock.notifyAll();
+
+                System.out.println(this);
+            }
+        });
     }
     @Override
     public String toString() {
